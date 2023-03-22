cf6b9ca03d7de3b705ec74de47e9ff4457dd98ae
BradDesktop <brad.martin@xdisplay.com>
2023-03-22 10:44:49 -0400

    gampad display

--- src/main.c
+++ src/main.c
@@ -4,6 +4,27 @@
 #include <emscripten/emscripten.h>
 #endif
 
+static const char *BUTTON_STRING[] = {
+    "unknown",
+    "left face up",
+    "left face right",
+    "left face down",
+    "left face left",
+    "right face up",
+    "right face right",
+    "right face down",
+    "right face left",
+    "left trig 1",
+    "left trig 2",
+    "right trig 1",
+    "right trig 2",
+    "mid left",
+    "mid",
+    "mid right",
+    "left thumb",
+    "right thumb",
+};
+
 static const int screenWidth = 450;
 static const int screenHeight = 450;
 
@@ -48,7 +69,22 @@ void DrawGame(void)
 {
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("HELLO WASM", GetScreenWidth() / 2 - MeasureText("HELLO WASM", 20) / 2, GetScreenHeight() / 2 - 10, 20, GRAY);
+    if (IsGamepadAvailable(0))
+    {
+        for (int i = 0; i <= 17; i++)
+        {
+            DrawText(BUTTON_STRING[i], 10, i * 20, 20, IsGamepadButtonDown(0, i) ? RED : BLACK);
+        }
+
+        DrawCircle(250 + (int)(GetGamepadAxisMovement(0, GAMEPAD_AXIS_LEFT_X) * 20),
+                   150 + (int)(GetGamepadAxisMovement(0, GAMEPAD_AXIS_LEFT_Y) * 20), 25, BLACK);
+        DrawCircle(350 + (int)(GetGamepadAxisMovement(0, GAMEPAD_AXIS_RIGHT_X) * 20),
+                   150 + (int)(GetGamepadAxisMovement(0, GAMEPAD_AXIS_RIGHT_Y) * 20), 25, BLACK);
+    }
+    else
+    {
+        DrawText("HELLO WASM", GetScreenWidth() / 2 - MeasureText("HELLO WASM", 20) / 2, GetScreenHeight() / 2 - 10, 20, GRAY);
+    }
     EndDrawing();
 }
 
