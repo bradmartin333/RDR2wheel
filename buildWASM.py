e8959c73313700829265f2d2a181986ceea33327
BradDesktop <brad.martin@xdisplay.com>
2023-03-22 09:43:15 -0400

    custom shell

--- buildWASM.py
+++ buildWASM.py
@@ -28,10 +28,6 @@ argParser.add_argument('-i', '--input', type=str,
                        default=cwd, help='input directory')
 argParser.add_argument('-o', '--output', type=str,
                        default=cwd, help='output directory')
-argParser.add_argument(
-    '-nb', '--nobuild', action='store_true', help='do not build WASM')
-argParser.add_argument(
-    '-nc', '--noclean', action='store_true', help='do not clean output .html')
 args = vars(argParser.parse_args())
 
 inputPath = os.path.join(args['input'], 'main.c')
@@ -40,29 +36,20 @@ if not os.path.exists(inputPath):
     exit(0)
 outputPath = os.path.join(args['output'], 'game.html')
 
-if not args['nobuild']:
-    raylibSrcDir = 'C:/raylib/raylib/src'
-    compilerArgs = "-Wall -std=c99 -D_DEFAULT_SOURCE -Wno-missing-braces -Wunused-result -Os -I. -I {0} -I {0}/external -L. -L {0} -s USE_GLFW=3 -s ASYNCIFY -s TOTAL_MEMORY=67108864 -s FORCE_FILESYSTEM=1 --shell-file {0}/shell.html {0}/web/libraylib.a -DPLATFORM_WEB -s EXPORTED_FUNCTIONS=[\"_free\",\"_malloc\",\"_main\"] -s EXPORTED_RUNTIME_METHODS=ccall".format(
-        raylibSrcDir)
-    build_cmd = "emcc -o {} {} {}".format(outputPath, inputPath, compilerArgs)
-    os.system(build_cmd)
+# Build WASM
+raylibSrcDir = 'C:/raylib/raylib/src'
+compilerArgs = "-Wall -std=c99 -D_DEFAULT_SOURCE -Wno-missing-braces -Wunused-result -Os -I. -I {0} -I {0}/external -L. -L {0} -s USE_GLFW=3 -s ASYNCIFY -s TOTAL_MEMORY=67108864 -s FORCE_FILESYSTEM=1 --shell-file ./src/shell.html {0}/web/libraylib.a -DPLATFORM_WEB -s EXPORTED_FUNCTIONS=[\"_free\",\"_malloc\",\"_main\"] -s EXPORTED_RUNTIME_METHODS=ccall".format(
+    raylibSrcDir)
+build_cmd = "emcc -o {} {} {}".format(outputPath, inputPath, compilerArgs)
+os.system(build_cmd)
 
 
-if not args['noclean']:
-    # Read the output .html
-    with open(outputPath, 'r') as f:
-        webpage = f.read()
-    soup = BeautifulSoup(webpage, 'html.parser')
-    # Replace head with cleanHead
-    head = soup.select('head')[0]
-    head.clear()
-    cleanHead = "<title>" + args['name'] + "</title> <meta charset=\"utf-8\"/> <meta content=\"text/html; charset=utf-8\" http-equiv=\"Content-Type\"/> <meta content=\"width=device-width\" name=\"viewport\"/> <style>body{font-family: arial; margin: 0; padding: none; background-color: " + args[
-        'color'] + "}.emscripten{padding-right: 0; margin-left: auto; margin-right: auto; display: block}div.emscripten{text-align: center}div.emscripten_border{border: none;}canvas.emscripten{border: 0 none; background: #000; width: min(100vw, 100vh);}</style>"
-    head.append(BeautifulSoup(cleanHead, 'html.parser'))
-    # Hide/delete some body elements
-    soup.select('div')[0]['style'] = 'display: none;'
-    soup.select('textarea')[0]['style'] = 'display: none;'
-    soup.select('a')[0].decompose()
-    # Overwrite .html
-    with open(outputPath, 'w') as file:
-        file.write(soup.prettify())
+# Change name and color in .html
+with open(outputPath, 'r') as f:
+    webpage = f.read()
+soup = BeautifulSoup(webpage, 'html.parser')
+soup.select('title')[0].string = args['name']
+soup.select('body')[0]['style'] = 'background-color: ' + args['color'] + ';'
+# Overwrite .html
+with open(outputPath, 'w') as file:
+    file.write(soup.prettify())
